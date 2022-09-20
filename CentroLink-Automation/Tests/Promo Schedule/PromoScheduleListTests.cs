fabba912db13b9484e089bc8a4bdfa8b0a7e6ce3
Benjamin Dagg <bdagg@DiamondGame.com>
2022-09-20 13:16:10 -0700

    Added test class for Promo Schedule List page

---
+++ CentroLink-Automation/Tests/Promo Schedule/PromoScheduleListTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Threading;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Globalization;
+
+namespace CentroLink_Automation
+{
+    public class PromoScheduleListTests : BaseTest
+    {
+
+        public LoginPage loginPage;
+
+        [SetUp]
+        public override async Task Setup()
+        {
+            base.Setup();
+
+            loginPage = new LoginPage(driver);
+        }
+
+
+        [TearDown]
+        public override async Task EndTest()
+        {
+            base.EndTest();
+        }
+    }
+}
