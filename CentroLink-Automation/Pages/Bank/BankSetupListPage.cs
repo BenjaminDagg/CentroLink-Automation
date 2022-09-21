2466d7397b7be04a2342ec8f0318c5da89164419
Benjamin Dagg <bdagg@DiamondGame.com>
2022-09-21 15:48:10 -0700

    Added BankSetupList page class and test class

---
+++ CentroLink-Automation/Pages/Bank/BankSetupListPage.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;   //DesiredCapabilities
+using System;
+using Appium;
+using OpenQA.Selenium.Appium;   //Appium Options
+using System.Threading;
+using OpenQA.Selenium;
+using System.Linq;
+using System.Collections.Generic;
+using CentroLink_Automation.Pages.Machine_Setup;
+using OpenQA.Selenium.Interactions;
+
+namespace CentroLink_Automation
+{
+    public class BankSetupListPage : DataGridPage
+    {
+
+        public override By DataGrid { get => By.ClassName("DataGrid"); }
+        public override By RowSelector { get => By.ClassName("DataGridRow"); }
+
+        public ByAccessibilityId AddBankButton;
+        public ByAccessibilityId EditBankButon;
+        public ByAccessibilityId ViewMachinesAssignedToBankButton;
+        public ByAccessibilityId RefreshButton;
+
+        public BankSetupListPage(WindowsDriver<WindowsElement> _driver) : base(_driver)
+        {
+            this.driver = _driver;
+
+            AddBankButton = new ByAccessibilityId("AddBank");
+            EditBankButon = new ByAccessibilityId("EditSelectedBank");
+            ViewMachinesAssignedToBankButton = new ByAccessibilityId("ViewMachinesAssignedToBank");
+            RefreshButton = new ByAccessibilityId("RefreshBankList");
+        }
+
+
+        public void ClickAddBank()
+        {
+            driver.FindElement(AddBankButton).Click();
+        }
+
+
+        public void ClickEditBank()
+        {
+            WindowsElement editBtn = driver.FindElement(EditBankButon);
+
+            if (editBtn.Enabled)
+            {
+                editBtn.Click();
+            }
+        }
+
+
+        public void ViewMachinesAssignedToBank()
+        {
+            WindowsElement viewMachBtn = driver.FindElement(ViewMachinesAssignedToBankButton);
+
+            if (viewMachBtn.Enabled)
+            {
+                viewMachBtn.Click();
+            }
+        }
+
+
+        public void Refresh()
+        {
+            driver.FindElement(RefreshButton).Click();
+        }
+    }
+}
---
+++ CentroLink-Automation/Tests/Bank/BankSetupListTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CentroLink_Automation
+{
+    public class BankSetupListTests : BaseTest
+    {
+
+
+
+        [SetUp]
+        public async Task Setup()
+        {
+
+        }
+
+
+        [TearDown]
+        public override async Task EndTest()
+        {
+            base.EndTest();
+
+            await LotteryRetailDatabase.ResetTestDeal();
+            await LotteryRetailDatabase.ExecuteRecommendDealForClosePrecedure();
+
+            foreach (var deal in DealsToReset)
+            {
+                await LotteryRetailDatabase.UpdateDealEnabled(deal, true);
+            }
+        }
+    }
+}
