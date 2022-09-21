2a466b5182e5557bb73e16c47f162403517eb542
unknown <bdagg@csumb.edu>
2022-09-20 21:42:00 -0700

    Added test class for AddPromoSchedule page

---
+++ CentroLink-Automation/Tests/Promo Schedule/AddPromoScheduleTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System.Threading;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Globalization;
+
+namespace CentroLink_Automation
+{
+    public class AddPromoScheduleTests : BaseTest
+    {
+
+        private LoginPage loginPage;
+        private PromoScheduleListPage promoList;
+        private AddPromoSchedulePage addPromo;
+
+
+        [SetUp]
+        public override async Task Setup()
+        {
+            base.Setup();
+
+            loginPage = new LoginPage(driver);
+            promoList = new PromoScheduleListPage(driver);
+            addPromo = new AddPromoSchedulePage(driver);
+        }
+
+
+        [TearDown]
+        public override async Task EndTest()
+        {
+            base.EndTest();
+
+            await LotteryRetailDatabase.ResetTestPromo();
+        }
+    }
+}
